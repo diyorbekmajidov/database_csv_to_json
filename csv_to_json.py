18b9eb18f02bd773821e86b986d439779b812540
878 <majidovdiyorbek741@gmail.com>
2022-09-20 17:00:11 +0500

    json py

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,3 +1,7 @@
+import read_data
+import json
+from tinydb import TinyDB 
+db = TinyDB('db.json')
 def csv_to_json(data:list)-> None:
     """
     Given csv list turn into tinydb database
@@ -7,3 +11,10 @@ def csv_to_json(data:list)-> None:
     Returns:
         None
     """
+    mobile=db.table('Mobile')
+    for i in data[1:len(data)]:
+        mobile.insert(({'model':i[1], 'company':i[2], 'price':i[3]}))
+m=read_data.read_csv('specifications.csv')
+
+z=csv_to_json(m)
+print(z)
